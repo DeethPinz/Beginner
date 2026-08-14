7294a9698b67b5ed923dbf6c52cbbcb3231e5a41
Dereck <dereckyaniel78@gmail.com>
2026-08-13 21:52:18 -0400

    Lecture 4: Loops

---
+++ Lesson 4/4-2.py
@@ -0,0 +1,5 @@
+for i in range(5):
+    print("Hello")
+
+for number in range(5):
+    print("Attempt: ", number + 1)
---
+++ Lesson 4/4-8.py
@@ -0,0 +1,25 @@
+numbers = [2,7,4,9,10,3]
+count = 0
+
+for n in numbers:
+    if n > 5:
+        count = count + 1
+
+print (count)
+
+numbers = [2,7,4,9]
+total = 0
+
+for n in numbers:
+    total = total + n
+
+print(total)
+
+numbers = [2, 7, 4, 9, 19, 3]
+found = None
+for n in numbers:
+    if n > 8:
+        found = n
+        break
+
+print(found)
